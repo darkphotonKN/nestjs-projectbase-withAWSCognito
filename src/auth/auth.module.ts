60f1b91bcf233823100b9f67cebc912e249cf9fd
nick.peng <nick.peng@cloud-interactive.com>
2023-02-06 16:03:37 +0800

    remove cognito

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,11 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PassportModule } from '@nestjs/passport';
-import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [ConfigModule, PassportModule.register({ defaultStrategy: 'jwt' })],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy],
+  providers: [AuthService],
 })
 export class AuthModule {}
